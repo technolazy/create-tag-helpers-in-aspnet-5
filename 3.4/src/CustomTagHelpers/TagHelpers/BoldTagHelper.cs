01b0cdb5cd183ef50cc2d3d0b52a32dc8c456de5
Jeremy McPeak <jwmcpeak@gmail.com>
2015-11-01 21:35:22 -0600

    more stuff

---
+++ 3.4/src/CustomTagHelpers/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomTagHelpers.TagHelpers
+{
+    public class BoldTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var classValue = context.AllAttributes["class"].Value;
+            var existingContent = await context.GetChildContentAsync();
+            existingContent.Append(" It is!");
+
+            output.TagName = "span";
+            output.Attributes.Clear();
+            output.Content.SetContent(existingContent);
+            output.PreContent.AppendEncoded($"<strong class='{classValue}'>");
+            output.PostContent.AppendEncoded("</strong>");
+
+        }
+    }
+}
